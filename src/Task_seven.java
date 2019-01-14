06c436917006597708c00d720f357c2ab26fe757
https://github.com/Manya-srt/Selenium.git <manya-srt@mail.ru>
2019-01-14 17:49:18 +0300

    Commit

--- src/Task_seven.java
+++ src/Task_seven.java
@@ -1,7 +1,7 @@
 
 public class Task_seven {
         public static void main(String args[]){
-            for (int i = 1; i<= 50; i++) {
+            for (int i = 1; i<= 50; i++) { //вывести на экран числа от 1 до 50.
                 System.out.print(i + " ");
             }
          }
