e505f82a4691f48cdc87247f909a8cf026ece7c5
https://github.com/Manya-srt/Selenium.git <manya-srt@mail.ru>
2019-01-14 17:48:20 +0300

    Initial commit

---
+++ src/Task_seven.java
@@ -0,0 +1,8 @@
+
+public class Task_seven {
+        public static void main(String args[]){
+            for (int i = 1; i<= 50; i++) {
+                System.out.print(i + " ");
+            }
+         }
+}
